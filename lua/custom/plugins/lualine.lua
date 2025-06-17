0ce9b82013d0d2caf1eca6c023e66ac0aa001e2e
Ali <89438079+AliBa1@users.noreply.github.com>
2025-06-17 11:23:51 -0700

    updated config

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -8,6 +8,14 @@ return {
         theme = 'auto',
         -- theme = 'gruvbox',
       },
+      sections = {
+        lualine_c = {
+          {
+            'filename',
+            path = 1, -- 0 = filename only, 1 = relative path, 2 = absolute path, 3 = ~ for home
+          },
+        },
+      },
     }
   end,
 }
